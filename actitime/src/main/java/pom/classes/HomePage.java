82f900b096996391b6f0983f4b1bfb0f614a7101
AshishMunde07 <86188106+AshishMunde07@users.noreply.github.com>
2021-06-21 23:38:44 +0530

    add logout
    added

--- actitime/src/main/java/pom/classes/HomePage.java
+++ actitime/src/main/java/pom/classes/HomePage.java
@@ -19,6 +19,8 @@ public class HomePage {
 	
 	@FindBy (xpath="//a[@href='/ent/logout.do']")
 	private WebElement logout;
+	@FindBy (xpath="//a[@href='/ent/logout.do']")
+	private WebElement logout;
 	//private WebDriver driver ;
 			
 	public HomePage(WebDriver driver)
